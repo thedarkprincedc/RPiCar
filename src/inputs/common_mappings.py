5fe931a896553652fb0415184128dcfc7ef9f5d8
Brett Mosley <brettmosley@hotmail.com>
2026-07-30 13:36:29 -0400

    added common mapping

---
+++ src/inputs/common_mappings.py
@@ -0,0 +1,31 @@
+DUALSENSE_TO_GENERIC_MAPPING = {
+    "buttons": {
+        "square": "a",
+        "cross": "b",
+        "circle": "a",
+        "triangle": "b",
+
+        # face buttons
+        "create": "share",
+        "options": "start",
+        "ps": "home"
+    }
+}
+
+XBOX_TO_GENERIC_MAPPING = {
+    "buttons": {
+        # stick button click
+        "lsb": "",
+        "rsb": "",
+
+        # face buttons
+        "view": "share",
+        "options": "start",
+        "guide": "home"
+    }
+}
+
+CONTROLLER_MAPPINGS = {
+    "DUALSENSE": DUALSENSE_TO_GENERIC_MAPPING,
+    "XBOXONE": XBOX_TO_GENERIC_MAPPING
+}
